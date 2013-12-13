8db6320bd090b3c4b270aa70ac513054c8668fb9
tpf <tpf@163.com>
2013-12-13 16:19:08 +0800

    mv dependencies/libs to library

--- ogre/dependencies/libfreeimage/Android.mk
+++ ogre/dependencies/libfreeimage/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libfreeimage
 LOCAL_MODULE_FILENAME := libfreeimage
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libFreeImage.a
+LOCAL_SRC_FILES := library/$(TARGET_ARCH_ABI)/libFreeImage.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
--- ogre/dependencies/libfreetype/Android.mk
+++ ogre/dependencies/libfreetype/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libfreetype
 LOCAL_MODULE_FILENAME := freetype
-LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libfreetype.a
+LOCAL_SRC_FILES := library/$(TARGET_ARCH_ABI)/libfreetype.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
